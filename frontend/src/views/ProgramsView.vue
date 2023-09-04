9713ad82475d1636935e9855a573f6040e9a0a3e
Taflnne15 <taflynnepetersen@gmail.com>
2023-09-04 10:07:22 +0200

    ImageIssue

--- frontend/src/views/ProgramsView.vue
+++ frontend/src/views/ProgramsView.vue
@@ -1,18 +1,19 @@
 <template>
     <div class="programs">
-  </div>
-  <div class="info">
-    <div class="container text-center">
+        <div class="info">
+        <div class="container text-center">
   <div class="row align-items-center">
     <div class="col">
-
-
-       </div>
+        <h1>Our Prestige Programs</h1>
+        <span class="text">Welcome to our <span class="span4">prestige programs</span> a captivating music program that transcends boundaries and melodies. Join us on a melodious journey through the <span class="span4">rich tapestry of musical genres and cultures.</span> Whether you're a seasoned music enthusiast or just beginning to explore the world of sound, our program promises to <span class="span4">ignite your passion for rhythm, melody, and harmony.</span> Get ready to immerse yourself in the enchanting world of music like never before.</span>
+    </div>
+  
     <div class="col">
-   
+     
     </div>
   </div>
 </div>
+        </div>
   </div>
 </template>
 
@@ -26,17 +27,28 @@
 .programs{
     width: 100%;
   height: 100vh;
-  /* background: red; */
-  background-image: url(https://i.postimg.cc/Y0KpghY4/gir-singing.jpg);
-  background-repeat: no-repeat;
+   background: lightblue; 
+   background-image: url(https://i.postimg.cc/Y0KpghY4/gir-singing.jpg);
+  background-repeat: no-repeat; 
   background-size: cover;
   display: flex;
   justify-content: center;
   align-items: center;
+
 }
 .info{
-    margin-top: 100px;
-    margin-bottom: 100px;
+height: 100vh;
 }
+.text{
+    color: black;
+ 
+  font-family: "Oswald", sans-serif;
+
+}
+.col{
+    margin-top:200px;
+   color: black; 
+}
+
 
 </style>
