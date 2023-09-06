f4215e481badf222be57cf33bfd43a80bf7db0c1
Taflnne15 <taflynnepetersen@gmail.com>
2023-09-06 15:51:46 +0200

    update

--- API/config/index.js
+++ API/config/index.js
@@ -3,11 +3,10 @@ const {createPool} = require("mysql")
 
 //Set up database connection
 const connection = createPool({
-    host:process.env.DB_HOST,
-    user:process.env.DB_USER,
-    password:process.env.DB_PASSWORD,
-    port:process.env.DB_PORT,
-    database:process.env.DB_DATABASE,
+    host:process.env.dbHost,
+    user:process.env.dbUser,
+    password:process.env.dbPwd,
+    database:process.env.dbName,
     multipleStatements: true,
     connectionLimit: 30,
 })
